42ece1e2624b7ec4dd4fe0f4c5e5db4005586019
Cyril Le Goas <cyril.legoas@gmail.com>
2022-08-04 13:46:21 +0200

    ADD css reset

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Router } from "./Routes/Router";
+import "./Css/styles.css";
 
 const root = createRoot(document.querySelector("#root"));
 root.render(<Router />);
