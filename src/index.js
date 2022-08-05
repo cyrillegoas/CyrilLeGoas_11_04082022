79d52aff4a9744a21df4fecc18a94c587a672a65
Cyril Le Goas <cyril.legoas@gmail.com>
2022-08-05 17:51:13 +0200

    CHANGE file structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
-import { Router } from "./Routes/Router";
-import "./Css/styles.css";
+import { Router } from "./routes/Router";
+import "./css/styles.css";
 
 const root = createRoot(document.querySelector("#root"));
 root.render(<Router />);
