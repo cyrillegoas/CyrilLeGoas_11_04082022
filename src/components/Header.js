7e2af9e89c81daad04a09d7d00d4482ab3b29224
Cyril Le Goas <cyril.legoas@gmail.com>
2022-08-05 17:33:19 +0200

    ADD header component

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import { HorizontalNav } from "./HorizontalNav";
+import { ConditionalWrapper } from "./HelperComponents";
+
+export const Header = (props) => {
+  const navItems = [
+    {
+      path: "/",
+      name: "Accueil",
+    },
+    {
+      path: "/about",
+      name: "A Propos",
+    },
+  ];
+
+  return (
+    <header className={styles.header}>
+      <Link to="/">
+        <ConditionalWrapper
+          isWrapped={props.isLogoH1}
+          wrapper={(children) => <h1>{children}</h1>}
+        >
+          <img
+            className={styles.header__logo}
+            src={props.logoPath}
+            alt="logo"
+          />
+        </ConditionalWrapper>
+      </Link>
+      <HorizontalNav navItems={navItems} />
+    </header>
+  );
+};
---
+++ src/components/HorizontalNav.js
@@ -0,0 +1,27 @@
+import { Link, useLocation } from "react-router-dom";
+import styles from "./HorizontalNav.module.css";
+
+export const HorizontalNav = (props) => {
+  let location = useLocation();
+
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.nav__list}>
+        {props.navItems.map((item) => (
+          <li className={styles.nav__listItem} key={item.name}>
+            <Link
+              className={`${styles.nav__link} ${
+                location.pathname === item.path
+                  ? styles[`nav__link--active`]
+                  : ""
+              }`}
+              to={item.path}
+            >
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
