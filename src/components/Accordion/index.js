e49de6d5f4f69eabf4951f5f9b8eb024623ccd4e
Cyril Le Goas <cyril.legoas@gmail.com>
2022-08-10 16:12:17 +0200

    ADD accordion component

---
+++ src/components/Accordion/index.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export const Accordion = ({ name, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      className={`${styles.accordion} ${
+        isOpen ? styles["accordion--open"] : ""
+      }`}
+    >
+      <button
+        className={styles.accordion__btn}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span className={styles.accordion__btnText}>
+          {name}
+          <img
+            className={styles.accordion__arrow}
+            src="images/arrow.svg"
+            alt=""
+          />
+        </span>
+      </button>
+      <div className={styles.accordion__content}>{children}</div>
+    </div>
+  );
+};
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,29 @@
 import { Layout } from "../components/Layout";
 import { Banner } from "../components/Banner";
+import { Accordion } from "../components/Accordion";
 
 export const About = (props) => (
   <Layout>
     <Banner image="images/background2.png" />
+    <Accordion name="Fiabilité">
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, hic
+      voluptate! Dicta officia, totam expedita obcaecati ex, minus consectetur,
+      nam illum vero odit voluptate inventore. Quam voluptatem ad fuga vitae!"
+    </Accordion>
+    <Accordion name="Respect">
+      "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+      comportement discriminatoire ou de perturbation du voisinage entraînera
+      une exclusion de notre plateforme."
+    </Accordion>
+    <Accordion name="Service">
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, hic
+      voluptate! Dicta officia, totam expedita obcaecati ex, minus consectetur,
+      nam illum vero odit voluptate inventore. Quam voluptatem ad fuga vitae!"
+    </Accordion>
+    <Accordion name="Responsabilité">
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, hic
+      voluptate! Dicta officia, totam expedita obcaecati ex, minus consectetur,
+      nam illum vero odit voluptate inventore. Quam voluptatem ad fuga vitae!"
+    </Accordion>
   </Layout>
 );
