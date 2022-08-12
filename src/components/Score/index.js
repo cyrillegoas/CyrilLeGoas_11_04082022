1349a2e07515b60c0fef072e345ca64166258850
Cyril Le Goas <cyril.legoas@gmail.com>
2022-08-12 15:22:20 +0200

    CHANGE for loop by Array method

--- src/components/Score/index.js
+++ src/components/Score/index.js
@@ -1,26 +1,12 @@
 import styles from "./index.module.css";
 
 export const Score = ({ score, maxScore }) => {
-  let stars = [];
-
-  for (let i = 0; i < score; i++) {
-    stars.push(
-      <img
-        className={styles.score__star}
-        src="/images/star_filled.svg"
-        alt="filled star"
-      />
-    );
-  }
-  for (let i = 0; i < maxScore - score; i++) {
-    stars.push(
-      <img
-        className={styles.score__star}
-        src="/images/star_empty.svg"
-        alt="empty star"
-      />
-    );
-  }
+  const stars = Array.from({ length: maxScore }, (item, index) => {
+    const modifier = index < score ? "filled" : "empty";
+    const imageSrc = `/images/star_${modifier}.svg`;
+    const imageAlt = `${modifier} star`;
+    return <img className={styles.score__star} src={imageSrc} alt={imageAlt} />;
+  });
 
   return <div className={styles.score}>{stars}</div>;
 };
