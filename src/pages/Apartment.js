36e357ee4fd018f58be42161e2ec5d06e732bf9a
Cyril Le Goas <cyril.legoas@gmail.com>
2022-08-12 18:51:43 +0200

    FIX no redirect to 404 in case of unvalid id

--- src/pages/Apartment.js
+++ src/pages/Apartment.js
@@ -1,19 +1,24 @@
-import { useContext } from "react";
-import { useParams } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import { ApartmentsContext } from "../ApartmentsContext";
 import { Layout } from "../components/Layout";
 import { Slider } from "../components/Slider";
 import { ApartmentInfo } from "../components/ApartmentInfo";
 
 export const Apartment = (props) => {
+  const navigate = useNavigate();
   const apartments = useContext(ApartmentsContext);
   const { apartmentId } = useParams();
   const ApartmentDetails = apartments.find((item) => item.id === apartmentId);
 
-  return (
+  useEffect(() => {
+    ApartmentDetails || navigate("/notfound", { replace: true });
+  });
+
+  return ApartmentDetails ? (
     <Layout page="apartment">
       <Slider pictures={ApartmentDetails.pictures} />
       <ApartmentInfo />
     </Layout>
-  );
+  ) : null;
 };
