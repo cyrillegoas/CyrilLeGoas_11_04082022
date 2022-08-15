e952272ed3218c58ac4b579e3b5e775f408ee9c4
Cyril Le Goas <cyril.legoas@gmail.com>
2022-08-15 15:32:12 +0200

    FIX stars alt text

--- src/components/Score/index.js
+++ src/components/Score/index.js
@@ -4,14 +4,8 @@ export const Score = ({ score, maxScore }) => {
   const stars = Array.from({ length: maxScore }, (item, index) => {
     const modifier = index < score ? "filled" : "empty";
     const imageSrc = `/images/star_${modifier}.svg`;
-    const imageAlt = `${modifier} star`;
     return (
-      <img
-        className={styles.score__star}
-        src={imageSrc}
-        alt={imageAlt}
-        key={index}
-      />
+      <img className={styles.score__star} src={imageSrc} alt="" key={index} />
     );
   });
 
